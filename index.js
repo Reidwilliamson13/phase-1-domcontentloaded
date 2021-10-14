376a0e8a6ccc4be14e8aa9bb24b3ddb90044dc77
ReidWilliamson <reidwilliamson2@gmail.com>
2021-10-14 19:08:34 -0400

    commit tree e93bd42acaba51c95a5254c240fa8d9023e3a7f1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let text = document.querySelector("#text");
+  text.textContent = "This is really cool!";
+});
